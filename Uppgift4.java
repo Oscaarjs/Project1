76b0bff5b5e0040034baf5b03963bf48a653acd0
Oscaarjs <37636054+Oscaarjs@users.noreply.github.com>
2018-03-21 22:39:50 +0100

    Update Uppgift4.java

--- Uppgift4.java
+++ Uppgift4.java
@@ -1,14 +1,16 @@
 package Proj1;
+import java.util.Arrays;
 
 public class Uppgift4 {
 
 	 public static int[] addAtIndex(int[] a, int x, int index) {
 		 if(a[index] != 0) {
-			 for(int i = index; i < a.length-1; i++) {
-				 a[index] = 0;	
-				 a[i+1] = a[i];
+			 for(int i = a.length-1; i > index ; i--) {
+				 
+				 a[i] = a[i-1];
 				 
 			 }
+			 a[index] = x;
 		 }
 		 else {
 			 a[index] = x;
@@ -18,5 +20,7 @@ public class Uppgift4 {
 	 
 	 public static void main(String[] cmdln) {
 		 int[] arr = {0,1,0,0,0,2,0,1,4,0};
+		 arr = addAtIndex(arr, 9, 5);
+		 System.out.println(Arrays.toString(arr));
 	 }
 }
